e058b73e63a8bd8c829145ce0c5809441a672419
Jeff Barry Norte <jeffbarrynorte@gmail.com>
2020-03-03 18:46:07 +0800

    Additional fixes for endround endpoint

--- app/Http/Controllers/SolidGamingController.php
+++ app/Http/Controllers/SolidGamingController.php
@@ -425,5 +425,76 @@ class SolidGamingController extends Controller
 		echo json_encode($response);
 	}
 
+	public function endPlayerRound(Request $request) 
+	{
+		$json_data = json_decode(file_get_contents("php://input"), true);
+
+		$response = [
+						"errorcode" =>  "PLAYER_NOT_FOUND",
+						"errormessage" => "The provided playerid don’t exist.",
+						"httpstatus" => "404"
+					];
+
+		$player_id = $json_data["playerid"];
+
+		$client_details = DB::table("players AS p")
+						 ->select('p.client_id', 'p.player_id', 'p.username', 'p.email', 'p.language', 'p.currency', 'pst.player_token' , 'pst.status_id', 'p.display_name', 'c.client_api_key', 'cat.client_token AS client_access_token', 'ce.player_details_url', 'ce.fund_transfer_url')
+						 ->leftJoin("player_session_tokens AS pst", "p.player_id", "=", "pst.player_id")
+						 ->leftJoin("clients AS c", "c.client_id", "=", "p.client_id")
+						 ->leftJoin("client_endpoints AS ce", "c.client_id", "=", "ce.client_id")
+						 ->leftJoin("client_access_tokens AS cat", "c.client_id", "=", "cat.client_id")
+						 ->where("p.player_id", $player_id)
+						 ->where("pst.status_id", 1)
+						 ->first();
+
+		if ($client_details) {
+			$client = new Client([
+			    'headers' => [ 
+			    	'Content-Type' => 'application/json',
+			    	'Authorization' => 'Bearer '.$client_details->client_access_token
+			    ]
+			]);
+			
+			$guzzle_response = $client->post($client_details->fund_transfer_url,
+			    ['body' => json_encode(
+			        	[
+						  "access_token" => $client_details->client_access_token,
+						  "hashkey" => md5($client_details->client_api_key.$client_details->client_access_token),
+						  "type" => "fundtransferrequest",
+						  "datetsent" => "",
+						  "gamedetails" => [
+						    "gameid" => "",
+						    "gamename" => ""
+						  ],
+						  "fundtransferrequest" => [
+								"playerinfo" => [
+								"token" => $client_details->player_token
+							],
+							"fundinfo" => [
+							      "gamesessionid" => "",
+							      "transactiontype" => "debit",
+							      "transferid" => "",
+							      "currencycode" => $client_details->currency,
+							      "amount" => 0
+							]
+						  ]
+						]
+			    )]
+			);
+
+			$client_response = json_decode($guzzle_response->getBody()->getContents());
+			
+			$response = [
+				"status" => "OK",
+				"currency" => $client_response->fundtransferresponse->currencycode,
+				"balance" => $client_response->fundtransferresponse->balance,
+			];
+		}
+
+		Helper::saveLog('playerdetails', 2, file_get_contents("php://input"), $response);
+		echo json_encode($response);
+
+	}
+
 
 }
