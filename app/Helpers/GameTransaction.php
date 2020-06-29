e777b6c593788067cba566e8885566fccf648d4f
Jeff Barry Norte <j.norte@welltreasureph.com>
2020-06-29 09:47:15 +0000

    Revert "Solid Gaming Transaction Switch"
    This reverts commit 91e13cb064fcd2ae06614e575807ac72cef172d3

--- app/Helpers/GameTransaction.php
+++ app/Helpers/GameTransaction.php
@@ -44,46 +44,6 @@ class GameTransaction
 		DB::table('game_transactions')->insert($trans_data);
 	}
 
-	public static function solid_save($method, $request_data, $game_data, $client_data, $player_data) {
-		/*var_dump($request_data); die();*/
-		$trans_data = [
-					"token_id" => $player_data->token_id,
-					"game_id" => $game_data->game_id,
-					"round_id" => $request_data["roundid"],
-					"income" => $request_data["income"]
-				];
-
-		switch ($method) {
-		    case "debit":
-					$trans_data["provider_trans_id"] = $request_data["transid"];
-					$trans_data["bet_amount"] = $request_data["amount"];
-					$trans_data["win"] = 0;
-					$trans_data["pay_amount"] = 0;
-					$trans_data["entry_id"] = 1;
-		        break;
-		    case "credit":
-			        $trans_data["provider_trans_id"] = $request_data["transid"];
-			        $trans_data["bet_amount"] = 0;
-			        $trans_data["win"] = 1;
-			        $trans_data["pay_amount"] = abs($request_data["amount"]);
-			        $trans_data["entry_id"] = 2;
-			        $trans_data["payout_reason"] = $request_data["reason"];
-		        break;
-		    case "rollback":
-		    		$trans_data["provider_trans_id"] = (array_key_exists('transid', $request_data) ? $request_data["transid"] : '');
-					$trans_data["bet_amount"] = 0;
-					$trans_data["win"] = 1;
-					$trans_data["pay_amount"] = $game_data->pay_amount;
-					$trans_data["entry_id"] = 3;
-					$trans_data["payout_reason"] = "Rollback of transaction ID: ".$game_data->game_trans_id;
-		        break;
-
-		    default:
-		}
-		/*var_dump($trans_data); die();*/
-		DB::table('solid_game_transactions')->insert($trans_data);
-	}
-
 	public static function update($method, $request_data, $game_data, $client_data, $player_data) {
 
 		$game_details = DB::table("game_transactions AS g")
@@ -94,7 +54,7 @@ class GameTransaction
 		$win = $game_details->win;
 		$pay_amount = $game_details->pay_amount;
 		$entry_id = $game_details->entry_id;
-
+		
 		if($request_data["amount"] > 0.00) {
 			$win = 1;
 			$pay_amount = $game_details->pay_amount + $request_data["amount"];
@@ -109,41 +69,8 @@ class GameTransaction
 		return ($update ? true : false);
 	}
 
-	public static function solid_update($method, $request_data, $game_data, $client_data, $player_data) {
-
-		$game_details = DB::table("game_transactions AS g")
-				 ->where("g.round_id", $request_data['roundid'])
-				 ->first();
-		
-		$income = $game_details->income; 
-		$win = $game_details->win;
-		$pay_amount = $game_details->pay_amount;
-		$entry_id = $game_details->entry_id;
-
-		if($request_data["amount"] > 0.00) {
-			$win = 1;
-			$pay_amount = $game_details->pay_amount + $request_data["amount"];
-			$income = $game_details->bet_amount - $pay_amount;
-			$entry_id = 2;
-		}
-
-        $update = DB::table('solid_game_transactions')
-                ->where('game_trans_id', $game_details->game_trans_id)
-                ->update(['pay_amount' => $pay_amount, 'income' => $income, 'win' => $win, 'entry_id' => $entry_id]);
-                
-		return ($update ? true : false);
-	}
-
 	public static function find($original_trans_id) {
 		$transaction_id = DB::table('game_transactions')
-								->where('provider_trans_id', $original_trans_id)
-								->first();
-
-		return ($transaction_id ? $transaction_id : false);
-	}
-
-	public static function solid_find($original_trans_id) {
-		$transaction_id = DB::table('solid_game_transactions')
 								->where('provider_trans_id', $original_trans_id)
 								->first();
 
@@ -160,27 +87,6 @@ class GameTransaction
 
 	public static function bulk_rollback($round_id) {
 		$transactions = DB::table('game_transactions')
-								->where('round_id', $round_id)
-								->where('entry_id', 1)
-								->get();
-
-		/*$transactions_ids_to_roll_back = [];*/
-		$transactions_to_roll_back = [];
-		foreach ($transactions as $key => $value) {
-			/*array_push($transactions_ids_to_roll_back, $value->game_trans_id);*/
-			$transactions_to_roll_back[$value->game_trans_id] = $value;
-		}
-
-		/*$end_round_result = DB::table('game_transactions')
-                ->where('round_id', $round_id)
-                ->whereIn('game_trans_id', $transactions_ids_to_roll_back)
-                ->update(['entry_id' => 3]);*/
-
-		return (count($transactions_to_roll_back) > 0 ? $transactions_to_roll_back : false);
-	}
-
-	public static function solid_bulk_rollback($round_id) {
-		$transactions = DB::table('solid_game_transactions')
 								->where('round_id', $round_id)
 								->where('entry_id', 1)
 								->get();
