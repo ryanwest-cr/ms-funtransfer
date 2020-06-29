4ee32dd081980654c65180c14b101de9f974fe3a
Jan-Joven Jumao-as <janjoven.jumaoas@gmail.com>
2020-06-29 15:53:12 +0800

    fix the language

--- app/Http/Controllers/GameLobby/GameLobbyController.php
+++ app/Http/Controllers/GameLobby/GameLobbyController.php
@@ -125,7 +125,7 @@ class GameLobbyController extends Controller
             else{
                 $ip_address = "127.0.0.1";
             }
-            $lang = $request->has("lang")?$request->language:"en";
+            $lang = $request->has("lang")?$request->lang:"en";
             if($token=Helper::checkPlayerExist($request->client_id,$request->client_player_id,$request->username,$request->email,$request->display_name,$request->token,$ip_address)){
                 if($request->input('game_provider')=="Iconic Gaming"){
                     $msg = array(
