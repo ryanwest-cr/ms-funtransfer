f13c1f69b3d53b3383ce4479322bc84dbeedc21e
Jan-Joven Jumao-as <janjoven.jumaoas@gmail.com>
2020-07-16 15:17:23 +0800

    added client_player_id on EDP and ICG

--- app/Http/Controllers/EDPController.php
+++ app/Http/Controllers/EDPController.php
@@ -121,6 +121,7 @@ class EDPController extends Controller
                                 "gameid" => "",
                                 "clientid" => $client_details->client_id,
                                 "playerdetailsrequest" => [
+                                    "client_player_id"=>$client_details->client_player_id,
                                     "token" => $client_details->player_token,
                                     "gamelaunch" => "false"
                                 ]]
