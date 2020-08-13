d0bfa933b889e18e63b03536ed72827bd4fd0f52
#marvin <marvinvillanea1@gmail.com>
2020-08-13 18:55:43 +0800

    marvin-commit

--- app/Helpers/GameLobby.php
+++ app/Helpers/GameLobby.php
@@ -381,8 +381,7 @@ class GameLobby{
         $client_details = ProviderHelper::getClientDetails('token',$data["token"]);
         $player_details = Providerhelper::playerDetailsCall($client_details->player_token);
         try{
-            $nonce = date('mdYHisu', strtotime('+8 hours')).'1';
-            Helper::saveLog('Booming nonce', 36, json_encode($data), $nonce);
+            $nonce = date('mdYHisu', strtotime('+8 hours'));
             $requesttosend = array (
                 'game_id' => $data["game_code"],
                 'balance' => $player_details->playerdetailsresponse->balance,
