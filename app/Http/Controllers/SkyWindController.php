f10157d7ab86beae0b598c89184e09c95205c557
unknown <riandraft@gmail.com>
2020-08-14 13:42:00 +0800

    SkyWind

--- app/Http/Controllers/SkyWindController.php
+++ app/Http/Controllers/SkyWindController.php
@@ -121,7 +121,7 @@ class SkyWindController extends Controller
       }
       
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
       curl_setopt($ch, CURLOPT_CAPATH, "/etc/letsencrypt/live/api-test.betrnk.games/privkey.pem");
       curl_setopt($ch, CURLOPT_CAPATH, "/etc/letsencrypt/live/api-test.betrnk.games/fullchain.pem");
       curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
