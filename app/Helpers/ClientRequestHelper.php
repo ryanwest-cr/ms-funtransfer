2b43937966b99ed81fe8f11c438dae186cdb35ed
Jan-Joven Jumao-as <janjoven.jumaoas@gmail.com>
2020-08-17 20:39:09 +0800

    Gametransaction ID Added as helper

--- app/Helpers/ClientRequestHelper.php
+++ app/Helpers/ClientRequestHelper.php
@@ -13,10 +13,17 @@ class ClientRequestHelper{
         $transaction = DB::table("game_transactions")
                     ->where("provider_trans_id",$provider_trans_id)
                     ->where("round_id",$roundId)->first();
+        if($transaction){
+            $transaction->game_trans_id = $transaction->game_trans_id;
+        }
+        else{
+            $transaction = DB::table("game_transaction_ext")->latest()->first();
+            $transaction->game_trans_id = $transaction->game_trans_id +1;
+        }
         $transaction_ext = DB::table("game_transaction_ext")->latest()->first();
         $data = array(
             "transferId" => $transaction_ext->game_trans_ext_id + 1,
-            "roundId" => $transaction
+            "roundId" => $transaction->game_trans_id
         );
         return $data;
     }
