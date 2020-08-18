8b8f0c43ae75d6939a2d3750e820641249c8f13d
Al <riandraft@gmail.com>
2020-08-19 00:11:55 +0800

    Bole Gaming

--- app/Http/Controllers/BoleGamingController.php
+++ app/Http/Controllers/BoleGamingController.php
@@ -421,7 +421,7 @@ class BoleGamingController extends Controller
 			                $round_id = $json_data->report_id;
 			                // TEST
 
-							$gamerecord  = ProviderHelper::createGameTransaction($token_id, $game_code, $bet_amount,  $pay_amount, $method, $win_or_lost, null, $payout_reason, $income, $provider_trans_id, $round_id);
+							$gamerecord  = ProviderHelper::createGameTransaction($token_id, $game_code, $bet_amount,  0, $method, $win_or_lost, null, $payout_reason, $income, $provider_trans_id, $round_id);
 							$game_transextension = ProviderHelper::createGameTransExtV2($gamerecord,$provider_trans_id, $round_id, $bet_amount, $game_transaction_type);
 
 							$client_response = ClientRequestHelper::fundTransfer($client_details,abs($pay_amount),$db_game_code,$db_game_name,$gamerecord,$game_transextension,$transaction_type);
