522840cce66a9c922ec8991326cc4559f35e4bb9
unknown <zhack001@gmail.com>
2020-09-24 15:58:13 +0800

    ygg

--- app/Http/Controllers/YGGController.php
+++ app/Http/Controllers/YGGController.php
@@ -452,6 +452,7 @@ class YGGController extends Controller
 
             $client_response = ClientRequestHelper::fundTransfer($client_details, $bet_amount, $game_details[0]->game_code, $game_details[0]->game_name, $game_trans_ext_v2, $getTrans[0]->game_trans_id, 'credit');
 
+            
             $response = array(
                 "code" => 0,
                 "data" => array(
