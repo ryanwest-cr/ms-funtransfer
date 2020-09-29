1e6b5c3f3c4109658803c6efba07be6c43a29806
Al <riandraft@gmail.com>
2020-09-29 20:23:10 +0800

    ALDEBUGGER

--- app/Http/Controllers/AlController.php
+++ app/Http/Controllers/AlController.php
@@ -10,6 +10,7 @@ use App\Helpers\ProviderHelper;
 use GuzzleHttp\Client;
 use App\Helpers\ClientRequestHelper;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 use Session;
 use Auth;
 use DB;
@@ -19,14 +20,12 @@ use DB;
  */
 class AlController extends Controller
 {
+
+    public $hashen = '$2y$10$37VKbBiaJzWh7swxTpy6OOlldjjO9zdoSJSMvMM0.Xi2ToOv1LcSi';
+
     public function index(Request $request){
 
       // $token = Helper::tokenCheck('n58ec5e159f769ae0b7b3a0774fdbf80');
-
-      // // dd($token .' ' .Helper::datesent());
-      // dd($token);
-
-        // https://asset-dev.betrnk.games/images/games/casino/habanero/Habanero_12Zodiacs_384x216.png
 
         $gg = DB::table('games as g')
             ->where('provider_id', $request->provider_id)
@@ -49,64 +48,97 @@ class AlController extends Controller
 
 
     public function checkCLientPlayer(Request $request){
-
-        $indentifer = $request->identifier;
-        $data = $request->data;
-
-        if(!isset($request->hashen)){
+        if(!$request->has('hashen')){
           return ['al' => 'OOPS RAINDROPS'];
         }
-
-        if($request->hashen != 'ALHananONTHELiNE'){
-           return ['al' => 'OOPS RAINDROPS'];
+        if(!Hash::check($request->hashen,$this->hashen)){
+          return ['al' => 'OOPS RAINDROPS'];
         }
-
-        $client_details = Providerhelper::getClientDetails($indentifer,  $data);
-        // $gg = Providerhelper::playerDetailsCall($client_details->player_token);
-        if($client_details == 'false'){
-          return ['al' => 'NO PLAYER FOUND'];
-        }else{
-          $client = new Client([
-              'headers' => [ 
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer '.$client_details->client_access_token
+        if($request->debugtype == 1){
+          $client_details = Providerhelper::getClientDetails($request->type,  $request->identifier);
+          if($client_details == 'false'){
+            return ['al' => 'NO PLAYER FOUND'];
+          }else{
+            $client = new Client([
+                'headers' => [ 
+                  'Content-Type' => 'application/json',
+                  'Authorization' => 'Bearer '.$client_details->client_access_token
+                ]
+            ]);
+            $datatosend = ["access_token" => $client_details->client_access_token,
+              "hashkey" => md5($client_details->client_api_key.$client_details->client_access_token),
+              "type" => "playerdetailsrequest",
+              "datesent" => Helper::datesent(),
+              "clientid" => $client_details->client_id,
+              "playerdetailsrequest" => [
+                "player_username"=>$client_details->username,
+                "client_player_id" => $client_details->client_player_id,
+                "token" => $client_details->player_token,
+                "gamelaunch" => true,
+                "refreshtoken" => $request->has('refreshtoken') ? true : false,
               ]
-          ]);
-          $datatosend = ["access_token" => $client_details->client_access_token,
-            "hashkey" => md5($client_details->client_api_key.$client_details->client_access_token),
-            "type" => "playerdetailsrequest",
-            "datesent" => Helper::datesent(),
-            "clientid" => $client_details->client_id,
-            "playerdetailsrequest" => [
-              "player_username"=>$client_details->username,
-              "client_player_id" => $client_details->client_player_id,
-              "token" => $client_details->player_token,
-              "gamelaunch" => true,
-              "refreshtoken" => $request->has('refreshtoken') ? true : false,
-            ]
-          ];
-          try{  
-            $guzzle_response = $client->post($client_details->player_details_url,
-                ['body' => json_encode($datatosend)]
-            );
-            $client_response = json_decode($guzzle_response->getBody()->getContents());
-            $client_response->request_body = $datatosend;
-            return json_encode($client_response);
-          }catch (\Exception $e){
-             $message = [
-              'request_body' => $datatosend,
-              'al' => $e->getMessage(),
-             ];
-             return $message;
-          } 
+            ];
+            try{  
+              $guzzle_response = $client->post($client_details->player_details_url,
+                  ['body' => json_encode($datatosend)]
+              );
+              $client_response = json_decode($guzzle_response->getBody()->getContents());
+              $client_response->request_body = $datatosend;
+              return json_encode($client_response);
+            }catch (\Exception $e){
+               $message = [
+                'request_body' => $datatosend,
+                'al' => $e->getMessage(),
+               ];
+               return $message;
+            } 
+          }
+        }elseif($request->debugtype == 2){
+            $gg = DB::table('seamless_request_logs');
+
+            if ($request->type == 'provider_id') {
+              $gg->where([
+                ["provider_id", "=", $request->identifier],
+              ]);
+            } 
+
+            if ($request->type == 'method_name') {
+              $gg->where([
+                ["method_name", "LIKE", "%$request->identifier%"],
+              ]);
+            } 
+
+            $result = $gg->limit($request->has('limit') ? $request->limit : 1);
+            $result = $gg->latest()->get(); // Added Latest (CQ9) 08-12-20 - Al
+            return $result ? $result : 'false';
+        }elseif($request->debugtype == 3){
+            $gg = DB::table('game_transaction_ext');
+            if ($request->type == 'game_trans_id') {
+              $gg->where([
+                ["game_trans_id", "=", $request->identifier],
+              ]);
+            } 
+            if ($request->type == 'game_trans_ext_id') {
+              $gg->where([
+                ["game_trans_ext_id", "=", $request->identifier],
+              ]);
+            } 
+            if ($request->type == 'round_id') {
+              $gg->where([
+                ["round_id", "=", $request->identifier],
+              ]);
+            } 
+            if ($request->type == 'provider_trans_id') {
+              $gg->where([
+                ["provider_trans_id", "=", $request->identifier],
+              ]);
+            } 
+            $result = $gg->limit($request->has('limit') ? $request->limit : 1);
+            $result = $gg->latest()->get(); // Added Latest (CQ9) 08-12-20 - Al
+            return $result ? $result : 'false';
         }
 
     }
-
-
-     
-   
-
 
 
 
