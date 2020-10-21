30f7950084c5e2eca5f2d9d28d981a9e7e3d2a94
Jan-Joven Jumao-as <janjoven.jumaoas@gmail.com>
2020-10-21 08:46:53 +0800

    Query optimization

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -283,7 +283,7 @@ class Helper
 		Helper::saveLog('TIMEgetGameTransaction(EVG)', 189, json_encode($game), "DB TIME");
 		Helper::saveLog('TIMEgetGameTransaction(EVG)', 189, json_encode(DB::getQueryLog()), "DB TIME");
 		$data = (array)$game;
-		return $data;
+		return $data[0];
 	}
 	public static function checkGameTransaction($provider_transaction_id,$round_id=false,$type=false){
 		if($type&&$round_id){
