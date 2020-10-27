ec569609e144ad712e32e2d007fc6c5d12390fbe
unknown <zhack001@gmail.com>
2020-10-28 02:10:28 +0800

    update_tidy

--- app/Http/Controllers/TidyController.php
+++ app/Http/Controllers/TidyController.php
@@ -269,6 +269,15 @@ class TidyController extends Controller
 		// }
 
 		//CHECKING WIN EXISTING game_transaction_ext IF WIN ALREADY PROCESS
+		// $transaction_check = ProviderHelper::findGameExt($transaction_uuid, 2,'transaction_id');
+		// if($transaction_check != 'false'){
+		// 	$data_response = [
+		// 		'error' => '99-011' 
+		// 	];
+		// 	Helper::saveLog('Tidy Win error', $this->provider_db_id, json_encode(file_get_contents("php://input")), $data_response);
+		// 	return $data_response;
+		// }
+		
 		try{
 			//get details on game_transaction
 			$bet_transaction = ProviderHelper::findGameTransaction($reference_transaction_uuid, 'transaction_id',1);
