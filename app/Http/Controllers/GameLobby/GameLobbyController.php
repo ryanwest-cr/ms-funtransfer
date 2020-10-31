d9f623515866db9a7f96e8b2be7e13c293296ce2
Al <riandraft@gmail.com>
2020-10-31 15:32:42 +0800

    Golden F

--- app/Http/Controllers/GameLobby/GameLobbyController.php
+++ app/Http/Controllers/GameLobby/GameLobbyController.php
@@ -117,17 +117,17 @@ class GameLobbyController extends Controller
             // }
             
             // Filters
-            if(ClientHelper::checkClientID($request->all()) != 200){
-                $log_id = Helper::saveLog('GAME LAUNCH', 99, json_encode($request->all()), 'FAILED LAUNCH '.$request->client_id);
-                $msg = array(
-                    "error_code" => ClientHelper::checkClientID($request->all()),
-                    "message" => ClientHelper::getClientErrorCode(ClientHelper::checkClientID($request->all())),
-                    "url" => config('providerlinks.tigergames').'/tigergames/api?msg='.ClientHelper::getClientErrorCode(ClientHelper::checkClientID($request->all())).'&id='.$log_id,
-                    "game_launch" => false
-                );
-                return response($msg,200)
-                ->header('Content-Type', 'application/json');
-            }
+            // if(ClientHelper::checkClientID($request->all()) != 200){
+            //     $log_id = Helper::saveLog('GAME LAUNCH', 99, json_encode($request->all()), 'FAILED LAUNCH '.$request->client_id);
+            //     $msg = array(
+            //         "error_code" => ClientHelper::checkClientID($request->all()),
+            //         "message" => ClientHelper::getClientErrorCode(ClientHelper::checkClientID($request->all())),
+            //         "url" => config('providerlinks.tigergames').'/tigergames/api?msg='.ClientHelper::getClientErrorCode(ClientHelper::checkClientID($request->all())).'&id='.$log_id,
+            //         "game_launch" => false
+            //     );
+            //     return response($msg,200)
+            //     ->header('Content-Type', 'application/json');
+            // }
 
            $solid_gamings = ['Solid Gaming', 'Booongo', 'Concept', 'Espresso', 'EvoPlay', 'GameArt', 'Habanero', 'MultiSlot', 'NetEnt', 'Oryx Gaming', 'Omi Gaming', 'Push Gaming', 'Revolver Gaming', 'RTG Asia', 'TPG', '1X2 Network', 'BetSoft', 'Booming', 'Leander', 'Lotus Gaming', 'No Limit City', 'One Touch', 'Quick Fire', 'Relax', 'Wazdan', 'Yggdrasil', 'Evolution Gaming', 'Golden Hero'];
 
