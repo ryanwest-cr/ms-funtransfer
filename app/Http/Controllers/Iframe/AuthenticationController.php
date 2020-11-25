b7fa790d5aceb10e168b4922fea3a13c48a78bac
unknown <riandraft@gmail.com>
2020-11-25 16:53:42 +0800

    No Multiple Session

--- app/Http/Controllers/Iframe/AuthenticationController.php
+++ app/Http/Controllers/Iframe/AuthenticationController.php
@@ -19,6 +19,7 @@ class AuthenticationController extends Controller
 	}
     public function checkTokenExist(Request $request){
         if($request->has("token")){
+            TransferWalletHelper::saveLog('checkTokenExist', 12, json_encode($request->all()), 'TRANSFER WALLET CHECK TOKEN');
             $token_data = DB::table('player_session_tokens')->where("player_token",$request->token)->first();
             if($token_data){
                 $response = array(
@@ -27,6 +28,7 @@ class AuthenticationController extends Controller
                     "exist" => true,
                 );
 
+                # Check IF token is Valid and has player
                 $token_identity = TransferWalletHelper::getClientDetails('token', $request->token);
                 if ($token_identity == 'false') {
                     $response = array(
@@ -37,27 +39,20 @@ class AuthenticationController extends Controller
                     return response($response, 200)
                     ->header('Content-Type', 'application/json');
                 }
+
+                # Check Multiple user Session
+                $session_count = SessionWalletHelper::isMultipleSession($token_identity->player_id, $request->token);
+                if ($session_count) {
+                    $response = array(
+                        "status" => "error",
+                        "message" => "Multiple Session Detected!",
+                        "exist" => true,
+                    );
+                }
+
                 $token = SessionWalletHelper::checkIfExistWalletSession($request->token);
                 if($token == false){ // This token doesnt exist in wallet_session
-                    $session_count = SessionWalletHelper::isMultipleSession($token_identity->player_id, $request->token);
-                    if ($session_count) {
-                        $response = array(
-                            "status" => "error",
-                            "message" => "Multiple Session Detected!",
-                            "exist" => true,
-                        );
-                    }
-                }else{
-                    $session_count = SessionWalletHelper::isMultipleSession($token_identity->player_id, $request->token);
-                    if ($session_count) {
-                        $response = array(
-                            "status" => "error",
-                            "message" => "Multiple Session Detected!",
-                            "exist" => true,
-                        );
-                    }else{
-                        SessionWalletHelper::createWalletSession($request->token, $request->all());
-                    }
+                    SessionWalletHelper::createWalletSession($request->token, $request->all());
                 }
                 return response($response,200)
                 ->header('Content-Type', 'application/json');
