e70ab982a5b33c5db95c0551e385620ce4bc0584
janjoven <janjoven.jumaoas@gmail.com>
2021-01-22 17:50:30 +0800

    add logs on db

--- app/Http/Controllers/EvolutionController.php
+++ app/Http/Controllers/EvolutionController.php
@@ -180,6 +180,7 @@ class EvolutionController extends Controller
                             "uuid"=>$data["uuid"],
                         );
                         //Helper::updateGameTransactionExt($transactionId,$client_response->requestoclient,$msg,$client_response);
+
                         Helper::saveLog('responseTime(EVG)', 12, json_encode(["type"=>"debitproccess","stating"=>$startTime,"response"=>microtime(true)]), ["response"=>microtime(true) - $startTime,"mw_response"=> microtime(true) - $startTime - $client_response_time,"clientresponse"=>$client_response_time]);
                         return response($msg,200)
                             ->header('Content-Type', 'application/json');
@@ -305,6 +306,7 @@ class EvolutionController extends Controller
                             "uuid"=>$data["uuid"],
                         );
                         // Helper::updateGameTransactionExt($transactionId,$client_response->requestoclient,$msg,$client_response);
+                        
                         Helper::saveLog('responseTime(EVG)', 12, json_encode(["type"=>"creditproccess","stating"=>$startTime,"response"=>microtime(true)]), ["response"=>microtime(true) - $startTime,"mw_response"=> microtime(true) - $startTime - $client_response_time,"clientresponse"=>$client_response_time]);
                         return response($msg,200)
                             ->header('Content-Type', 'application/json');
