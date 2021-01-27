70951932652629f26746d005d2b1b4522cfecc4c
janjoven <janjoven.jumaoas@gmail.com>
2021-01-27 14:36:30 +0800

    change all the enpoints and base_uri

--- config/services.php
+++ config/services.php
@@ -3,6 +3,7 @@
 
 return [
     'game_transaction' => [
-        'base_uri' => 'http://localhost:8889'
+        //'base_uri' => 'http://localhost:8889'
+        'base_uri' => 'https://svr-test01.betrnk.games/'
     ],
 ];
